c8cb68646aacb83c336a66e564b0c9d60fa7f57b
ALONGOD <alongoddard1@gmail.com>
2024-03-15 17:08:51 +0200

    Add render function

--- meme-controller.js
+++ meme-controller.js
@@ -13,5 +13,21 @@ renderMeme()
 function renderMeme(elImg) {
     // var elImg = document.querySelector('img')
     gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height)
+    drawText('hi there', 150, 150)
 
+}
+
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'orange'
+
+    gCtx.fillStyle = 'lightsteelblue'
+
+    gCtx.font = '45px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
 }
