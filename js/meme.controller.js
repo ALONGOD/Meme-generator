a4dcfcebfc4d5ea181e1fbf44a7d935232d1d4ba
ALONGOD <alongoddard1@gmail.com>
2024-09-08 09:38:44 +0300

    Code cleanup

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -131,7 +131,6 @@ function renderMeme() {
 
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
-        // Draw meme lines
         meme.lines.forEach((line, idx) => {
             if (idx === meme.selectedLineIdx) {
                 const textWidth = getTextWidth(line.txt, line.size, ' Montserrat')
@@ -252,39 +251,25 @@ function insertEmoji(emoji) {
 }
 
 function onShareFacebook() {
-    // Gets the image from the canvas
     const imgDataUrl = gCanvas.toDataURL('image/jpeg')
 
     function onSuccess(uploadedImgUrl) {
-        // Handle some special characters
         const url = encodeURIComponent(uploadedImgUrl)
         window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}&t=${url}`)
     }
 
-    // Send the image to the server
     doUploadImg(imgDataUrl, onSuccess)
 }
 
-// Upload the image to a server, get back a URL 
-// call the function onSuccess when done
 function doUploadImg(imgDataUrl, onSuccess) {
-    // Pack the image for delivery
     const formData = new FormData()
     formData.append('img', imgDataUrl)
 
-    // Send a post req with the image to the server
     const XHR = new XMLHttpRequest()
     XHR.onreadystatechange = () => {
-        // If the request is not done, we have no business here yet, so return
         if (XHR.readyState !== XMLHttpRequest.DONE) return
-        // if the response is not ok, show an error
         if (XHR.status !== 200) return console.error('Error uploading image')
         const { responseText: url } = XHR
-        // Same as
-        // const url = XHR.responseText
-
-        // If the response is ok, call the onSuccess callback function, 
-        // that will create the link to facebook using the url we got
         console.log('Got back live url:', url)
         onSuccess(url)
     }
